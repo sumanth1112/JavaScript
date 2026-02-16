950f53aa94cb3d97a4b868ad7fd70bffb94c2426
sumanth1112 <thimmareddysumanthreddy@gmail.com>
2026-02-16 10:20:23 +0530

    update

---
+++ file33.js
@@ -0,0 +1,13 @@
+const student = {
+    name: "john",
+    age: 21
+}
+
+// console.log(student.name)
+// console.log(student.age)
+
+//Destructuring
+
+const {name, age} = student
+console.log(name)
+console.log(age)
