586d984ae1904b8d7475f684424bc59c75721ba0
sumanth1112 <thimmareddysumanthreddy@gmail.com>
2026-02-16 11:20:10 +0530

    update

--- file35.js
+++ file35.js
@@ -15,3 +15,10 @@ const names = ["Suryakanth", "Ajay", "Sumanth", "Satyam"]
 // console.log(names.sort())
 // console.log(names.reverse())
 
+const numbers = [15, 2, 1, 8, 3, 2]
+// console.log(numbers.sort((a, b) => a-b))
+// console.log(numbers.sort((a, b) => b-a))
+// console.log(numbers.indexOf(2))
+// console.log(numbers.lastIndexOf(2))
+// console.log(numbers.includes(3))
+console.log(numbers.slice(2, 5))
