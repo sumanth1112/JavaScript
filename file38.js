b5184d83f7f04b317bedf89591763f963b832de8
sumanth1112 <thimmareddysumanthreddy@gmail.com>
2026-02-16 13:31:39 +0530

    update

--- file38.js
+++ file38.js
@@ -1,15 +1,32 @@
-let d = new Date()
+// let d = new Date()
+// console.log(d)
+
+// get methods
+
+// console.log(d.getDate())
+// console.log(d.getMonth())
+// console.log(d.getFullYear())
+// console.log(d.getHours())
+// console.log(d.getMinutes())
+// console.log(d.getSeconds())
+// console.log(d.getMilliseconds())
+// console.log(d.getDay())
+// console.log(d.getTime())
+// console.log(Date.now())
+
+//  let d = new Date(2026, 1, 1, 10, 10, 10)
+//  console.log(d)
+// let d = new Date("February 16, 2025 10:10:10")
+// console.log(d)
+// console.log(d.getMonth())
+
+const d = new Date()
+d.setFullYear(2025)
+d.setMonth(11)
+d.setDate(21)
+d.setHours(10)
+d.setMinutes(10)
+d.setSeconds(10)
+d.setMilliseconds(1)
+d.setTime(999999999)
 console.log(d)
-
-//get methods
-
-console.log(d.getDate())
-console.log(d.getMonth())
-console.log(d.getFullYear())
-console.log(d.getHours())
-console.log(d.getMinutes())
-console.log(d.getSeconds())
-console.log(d.getMilliseconds())
-console.log(d.getDay())
-console.log(d.getTime())
-console.log(Date.now())
