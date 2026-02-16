5d473c40ebcaf44ddfdfa7273beb24db2012fa4a
sumanth1112 <thimmareddysumanthreddy@gmail.com>
2026-02-16 11:06:23 +0530

    update

---
+++ file35.js
@@ -0,0 +1,17 @@
+const names = ["Suryakanth", "Ajay", "Sumanth", "Satyam"]
+// names.push("Vivek")
+// console.log(names)
+
+// names.pop()
+// console.log(names)
+
+// names.shift()
+// console.log(names)
+
+// names.unshift("Vivek")
+// console.log(names)
+//shift and unshift is slower because of rearrangement
+
+// console.log(names.sort())
+// console.log(names.reverse())
+
