2246863b69cba0e79c4bae96750a21ad1ab80f5d
sumanth1112 <thimmareddysumanthreddy@gmail.com>
2026-02-16 10:53:19 +0530

    update

---
+++ file34.js
@@ -0,0 +1,11 @@
+function response({name, email, role}) {
+    console.log(name)
+    console.log(email)
+    console.log(role)
+}
+const user = {
+    name:"john",
+    email: "john@gmail.com",
+    role: "user"
+}
+response(user)
