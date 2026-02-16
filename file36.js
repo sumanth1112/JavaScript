aef99ef7ff056537c3fefa83c268594ad219d9ae
sumanth1112 <thimmareddysumanthreddy@gmail.com>
2026-02-16 11:27:16 +0530

    update

---
+++ file36.js
@@ -0,0 +1,10 @@
+let num = 10.75
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))
+// console.log(Math.round(num))
+// console.log(num.toFixed(2))
+
+// console.log(isNaN(num))
+// console.log(Math.random())
+// console.log(Math.max(5, 7, 1, 8))
+console.log(Math.pow(10, 2))
